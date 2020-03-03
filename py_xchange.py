7c31f2cf29e9afc64337c81a1a5f5d93b0142698
marshki <marshall@morethantomorrow.org>
2020-03-03 09:53:24 -0500

    consistent formatting

--- py_xchange.py
+++ py_xchange.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python 3
 
-"""Placholder.
+"""Currency conversion utility (in progress).
 """
+
 from forex_python.converter import get_rate
 
-EUR_TO_US=get_rate('EUR', 'USD')
+EUR_TO_USD=get_rate('EUR', 'USD')
 
-
-#EUR_TO_US = 1.11
+# Sample exchange rate.
+# EUR_TO_USD = 1.11
 
 def convert_usd_to_eur(usd):
     """Convert USD to EUR.
     """
-    return round(usd/EUR_TO_US, 4)
+    return round(usd/EUR_TO_USD, 4)
 
 def convert_eur_to_usd(eur):
     """Convert EUR to USD.
     """
-    return round(eur*EUR_TO_US, 4)
+    return round(eur*EUR_TO_USD, 4)
 
 print(convert_usd_to_eur(.25))
 print(convert_usd_to_eur(1))
