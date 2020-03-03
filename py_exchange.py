5c1c997f72c4f4ad07b7f2bbcb7ad6461d4867a5
marshki <marshall@morethantomorrow.org>
2020-03-02 23:31:03 -0500

    add function

--- py_exchange.py
+++ py_exchange.py
@@ -5,18 +5,22 @@
 
 EUR_TO_US = 1.11
 
-def convert_usd_to_euro(usd):
+def convert_usd_to_eur(usd):
     """Convert USD to EUR.
     """
-    return round(usd/1.11, 4)
+    return round(usd/EUR_TO_US, 4)
 
-print(convert_usd_to_euro(.01))
-print(convert_usd_to_euro(.05))
-print(convert_usd_to_euro(.10))
-print(convert_usd_to_euro(.25))
-print(convert_usd_to_euro(1))
-print(convert_usd_to_euro(5))
-print(convert_usd_to_euro(10))
-print(convert_usd_to_euro(20))
-print(convert_usd_to_euro(50))
-print(convert_usd_to_euro(100))
+def convert_eur_to_usd(eur):
+    """Convert EUR to USD.
+    """
+    return round(eur*EUR_TO_US, 4)
+
+print(convert_usd_to_eur(.25))
+print(convert_usd_to_eur(1))
+print(convert_usd_to_eur(10))
+print(convert_usd_to_eur(20))
+
+print(convert_eur_to_usd(.25))
+print(convert_eur_to_usd(1))
+print(convert_eur_to_usd(10))
+print(convert_eur_to_usd(20))
