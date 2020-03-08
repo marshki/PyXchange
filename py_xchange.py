62be1023f3ffcc1f67f5efa14f70f6dc9eee6d5e
marshki <marshall@morethantomorrow.org>
2020-03-07 22:09:07 -0500

    add argparse function

--- py_xchange.py
+++ py_xchange.py
@@ -3,12 +3,24 @@
 """Currency conversion utility (in progress).
 """
 
+import argparse
 from forex_python.converter import get_rate
 
-EUR_TO_USD=get_rate('EUR', 'USD')
+EUR_TO_USD = get_rate('EUR', 'USD')
 
 # Sample exchange rate.
 # EUR_TO_USD = 1.11
+
+def parse_cli_args():
+    """Define parser w/arguments.
+    """
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--eur-to-usd", help="EUR to USD", nargs='+', type=float)
+    group.add_argument("--usd-to-eur", help="USD to EUR", nargs='+', type=float)
+    args = parser.parse_args()
+    return args
 
 def convert_usd_to_eur(usd):
     """Convert USD to EUR.
