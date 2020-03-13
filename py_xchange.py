5dfa4ddca5d1ac1ed7e771ab1b52344810e74b3e
marshki <marshall@morethantomorrow.org>
2020-03-13 07:43:41 -0400

    add arg parsing-WIP

--- py_xchange.py
+++ py_xchange.py
@@ -11,16 +11,6 @@ EUR_TO_USD = get_rate('EUR', 'USD')
 # Sample exchange rate.
 # EUR_TO_USD = 1.11
 
-def parse_cli_args():
-    """Define parser w/arguments.
-    """
-
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--eur-to-usd", help="EUR to USD", nargs='+', type=float)
-    group.add_argument("--usd-to-eur", help="USD to EUR", nargs='+', type=float)
-    args = parser.parse_args()
-    return args
 
 def convert_usd_to_eur(usd):
     """Convert USD to EUR.
@@ -32,12 +22,20 @@ def convert_eur_to_usd(eur):
     """
     return round(eur*EUR_TO_USD, 4)
 
-print(convert_usd_to_eur(.25))
-print(convert_usd_to_eur(1))
-print(convert_usd_to_eur(10))
-print(convert_usd_to_eur(20))
+def parse_cli_args():
+    """Define parser w/arguments.
+    """
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--usd-to-eur", help="USD to EUR", nargs='+', type=float)
+    group.add_argument("--eur-to-usd", help="EUR to USD", nargs='+', type=float)
+    args = parser.parse_args()
+    return args
 
-print(convert_eur_to_usd(.25))
-print(convert_eur_to_usd(1))
-print(convert_eur_to_usd(10))
-print(convert_eur_to_usd(20))
+if __name__ == '__main__':
+    args = parse_cli_args()
+
+    if args.usd_to_eur:
+        args.usd_to_eur, convert_usd_to_eur, 'usd', 'eur'
+    elif args.convert_eur_to_usd:
+        args.convert_eur_to_usd, convert_eur_to_usd, 'eur', 'usd'
