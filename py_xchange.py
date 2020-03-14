fddada7f4b3ee9bda906028ef002b522ad0eec78
marshki <marshall@morethantomorrow.org>
2020-03-14 11:30:17 -0400

    a kasha. please disregard

--- py_xchange.py
+++ py_xchange.py
@@ -8,10 +8,6 @@ from forex_python.converter import get_rate
 
 EUR_TO_USD = get_rate('EUR', 'USD')
 
-# Sample exchange rate.
-# EUR_TO_USD = 1.11
-
-
 def convert_usd_to_eur(usd):
     """Convert USD to EUR.
     """
@@ -22,20 +18,29 @@ def convert_eur_to_usd(eur):
     """
     return round(eur*EUR_TO_USD, 4)
 
+#print(convert_usd_to_eur(10))
+#print(convert_eur_to_usd(5))
+
 def parse_cli_args():
     """Define parser w/arguments.
     """
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--usd-to-eur", help="USD to EUR", nargs='+', type=float)
-    group.add_argument("--eur-to-usd", help="EUR to USD", nargs='+', type=float)
+    group.add_argument("--usd-to-eur", help="usd to eur", nargs='+', type=float)
+    group.add_argument("--eur-to-usd", help="eur to usd", nargs='+', type=float)
     args = parser.parse_args()
     return args
+
+def conversion_table(col1
+
+
 
 if __name__ == '__main__':
     args = parse_cli_args()
 
     if args.usd_to_eur:
         args.usd_to_eur, convert_usd_to_eur, 'usd', 'eur'
-    elif args.convert_eur_to_usd:
-        args.convert_eur_to_usd, convert_eur_to_usd, 'eur', 'usd'
+    elif args.eur_to_usd:
+        args.eur_to_usd, convert_eur_to_usd, 'eur', 'usd'
+    else:
+        print('Past the args') 
