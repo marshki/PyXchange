4daf98886e66c0410cf9e78a8aa7ca08daebfb83
marshki <marshall@morethantomorrow.org>
2020-03-14 15:26:49 -0400

    change column character

--- py_xchange.py
+++ py_xchange.py
@@ -29,12 +29,12 @@ def parse_cli_args():
     return args
 
 def conversion_table(col1, col2_func, c1_hdr, c2_hdr):
-    """Formatted table with (2) columns and (2) headers.
+    """Formatted table with two (2) columns and two (2) headers.
     """
 
     print('+------------+------------+')
     print('| {:^10} | {:^10} |'.format(c1_hdr, c2_hdr))
-    print('+------------+------------+')
+    print('+============+============+')
     for x_value in col1:
         y_value = col2_func(x_value)
         print('| {:10.4f} | {:10.4f} |'.format(x_value, y_value))
